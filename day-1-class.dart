592daa1f60d2c95d79e3a7e3ea5d618eea368420
Adisa Taiwo Joshua <adisataiwojoshua@gmail.com>
2021-04-15 16:57:46 +0100

    Introduction Basic Dart Programming language

---
+++ day-1-class.dart
@@ -0,0 +1,156 @@
+
+Day one curriculum
+
+/**
+ * 
+ * Listing some BASIC KEYWORDS in Dart
+ * 
+ * 
+ * **/
+/*
+class-void 
+else //Hafis
+ new //Sandra
+  static //Ayodele
+  import //Chibuzor
+  final //Abdullahi
+  const-main //Adeniyi
+  va-do while //David A
+  child //Simi Pam
+  while //Olatunde
+  enum-null-override-extends-implement // David F.
+    return-super //Joshua
+    
+    */
+
+/***
+ * int-String Abdullahi 
+ * bool - Adeniyi
+ * double - Ayodele
+ * List - Hafis
+ * Map - sandra
+ * 
+ * 
+ * Var
+ * dynamic
+ * fianl
+ * const
+ * 
+ * ----------
+ * 
+ * 
+ * 
+ * */
+    
+
+//create a DART PROGRAMMING that says HELLO SBSC FLUTTER DEVS!!!!!
+
+/*
+ * Dart is STRONGLY Typed but Strictly 
+ * */
+
+
+//main METHOD in Dart is the entry point 
+ 
+
+ void main() {
+   //Variable declaration and initialization - dart
+   
+    
+   
+   
+   //const: when a variable is marked as const, u can't reinitialize the value secondly, every variable marked with const MUST be initialized at declaration.
+   
+   const String abudllahi  ="HELLO SBSC FLUTTER DEVS";  
+   //IDENTIFIER: it is used to reference the locaTION OF YOUR VARIABLE (VALUES STORED IN A CONTAINER ) in memory.
+   
+   
+   int age = 50; //Number 
+   double amount = 1000.00; // Number 
+    
+      
+   
+   
+   print(abudllahi);//concatination 
+   
+   
+   //Variable Declarations 
+   int studentCount;
+   
+   
+   //DART Operators 
+   /*
+    * =====arithmetic operators
+    * + (Addtion ) Ayodele
+    * % (Modulus ) hafis 
+    * - (subtraction) Sandra
+    * / (Division) olatunde
+    * 
+    * * (multiplication) Simi 
+    * 
+    * 
+    * == === Relational Operator 
+    * == (equals to)Chibuzor
+    * != 
+    * 
+    *  * >
+    * <
+    * >=
+    * 
+    * ===== Logical operators 
+    * &&
+    * ||
+   
+    *Unary Operatory 
+    +=
+    -=
+    /=
+    
+    != 
+        * 
+    * */
+   
+   
+   //  Grading the class using Operators ....eg
+   
+   //Arithmetic 
+   int result = 0;
+   int classGrade = 90;
+   int totalclassScore = 100;
+    
+   
+   
+  //left operand   Assigmnet operator //right operand
+//    int result       =             classGrade * totalclassScore;
+   
+   
+   // Modulus operator helps you return the remainder 
+//    int result = classGrade % 2;
+  // double result = classGrade / 2; // There is going to be a floating //point (Ayodele )
+   
+   //Unary operator 
+   result += classGrade;
+   
+   
+   
+   //relational operator 
+   bool resultType =classGrade == totalclassScore;
+       
+   print(resultType);
+   
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
